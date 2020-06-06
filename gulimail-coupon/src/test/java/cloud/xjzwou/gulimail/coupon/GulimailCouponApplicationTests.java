d95227deabfa58c97a096c5d53c0d999c89b14c9
asdfghxjz <330984590@qq.com>
2020-06-06 14:32:28 +0800

    first commit

---
+++ gulimail-coupon/src/test/java/cloud/xjzwou/gulimail/coupon/GulimailCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.xjzwou.gulimail.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimail-product/src/test/java/cloud/xjzown/gulimail/product/GulimailProductApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.xjzown.gulimail.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
